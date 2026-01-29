012fb28d196800782ad5ccbe617797b8d0c6adb8
Rábai Miklós <rabaimiklos2001@gmail.com>
2026-01-29 13:33:39 +0100

    Második osztály kész

---
+++ RM0129/Masodik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RM0129
+{
+	internal class Masodik
+	{
+		List<Adat> List;
+
+		public Masodik(List<Adat> list)
+		{
+			List = list;
+		}
+	}
+}
