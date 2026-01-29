25dc843f154293b241bd5dda5ed344f18e593b51
Rábai Miklós <rabaimiklos2001@gmail.com>
2026-01-29 13:47:37 +0100

    Harmadik osztály kész

---
+++ RM0129/Harmadik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RM0129
+{
+	internal class Harmadik
+	{
+		Dictionary<Adat, int> stat;
+
+		public Harmadik(Dictionary<Adat, int> stat)
+		{
+			this.stat = stat;
+		}
+	}
+}
